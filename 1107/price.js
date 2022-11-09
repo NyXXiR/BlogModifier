df3ed52fae90aa9f1dfcd8772869160f1a1c9bf7
NyXXiR <lgh0334@gmail.com>
2022-11-09 16:12:20 +0900

    Initial commit

---
+++ 1107/price.js
@@ -0,0 +1,14 @@
+const space = document.querySelectorAll(".caption");
+const price = [];
+
+const priceOfFruit = [5000, 3000, 7000];
+
+function priceSupplyer() {
+  foreach.price.push(priceOfFruit);
+}
+
+console.log(price);
+
+상품명;
+설명텍스트;
+가격;
