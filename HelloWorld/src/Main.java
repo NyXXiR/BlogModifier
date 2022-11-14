3071e1d7f3ca482c4344851fef701b1fbe446009
NyXXiR <lgh0334@gmail.com>
2022-11-14 09:47:21 +0900

    .
    .

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
+
+// test case number = C
+//
+// student num N, score[a, b, c , d, e... (N개)
+// print 해라. 각 케이스마다 평균을 넘는 학생의 비율을. 소수점 셋째 자리까지 적용
