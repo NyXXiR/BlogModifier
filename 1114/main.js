2de1adb25f24965bbb532f4d1fe225f740169e83
NyXXiR <lgh0334@gmail.com>
2022-11-18 17:49:43 +0900

    .
    .

--- 1114/main.js
+++ 1114/main.js
@@ -3,8 +3,17 @@ let addButton = document.querySelector("#add-button");
 let tabs = document.querySelectorAll("#task-tabs div");
 
 var taskList = [];
+let mode = "all";
+let filterList = [];
 
 addButton.addEventListener("click", addTask);
+
+//이부분 중요
+for (let j = 1; j < tabs.length; j++) {
+  tabs[j].addEventListener("click", function (event) {
+    filter(event);
+  });
+}
 
 function addTask() {
   let task = {
@@ -14,30 +23,40 @@ function addTask() {
   };
 
   taskList.push(task);
+  taskInput.value = null;
   console.log(taskList);
   render();
 }
 
 function render() {
+  let list = [];
+  if (mode == "all") {
+    list = taskList;
+  } else {
+    list = filterList;
+  }
+
+  console.log("check:" + filterList);
+
   let resultHTML = "";
-  for (i = 0; i < taskList.length; i++) {
-    if (taskList[i].isComplete == true) {
+  for (i = 0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
       resultHTML += `
       <div class="task">
-        <div class="task-done">${taskList[i].taskContent}</div>
+        <div class="task-done">${list[i].taskContent}</div>
         <div>
-          <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-          <button id="delete-button">Delete</button>
+          <button onclick="toggleComplete('${list[i].id}')">Check</button>
+          <button onclick="deleteTask('${list[i].id}')">Delete</button>
         </div>
       </div>
       `;
     } else {
       resultHTML += `
       <div class="task">
-        <div>${taskList[i].taskContent}</div>
+        <div>${list[i].taskContent}</div>
         <div>
-          <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-          <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+          <button onclick="toggleComplete('${list[i].id}')">Check</button>
+          <button onclick="deleteTask('${list[i].id}')">Delete</button>
         </div>
       </div>
       `;
@@ -50,6 +69,7 @@ function randomIDGenerate() {
   return Math.random().toString(36).substr(2, 9);
 }
 
+//얘를 바꿔줘야 함
 function toggleComplete(id) {
   for (let i = 0; i < taskList.length; i++) {
     if (taskList[i].id == id) {
@@ -59,15 +79,64 @@ function toggleComplete(id) {
     }
   }
   render();
+
   console.log(taskList);
 }
 
-function deleteTask(id) {
-  for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].id == id) {
-      taskList.splice(i, 1);
-      break;
+function filter(event) {
+  // console.log("let me check", event.target.id);
+
+  mode = event.target.id;
+  filterList = [];
+  if (mode == "all") {
+    for (let i = 0; i < taskList.length; i++) {
+      filterList.push(taskList[i]);
+    }
+    render();
+  } else if (mode == "ongoing") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == false) {
+        filterList.push(taskList[i]);
+      }
+    }
+  } else if (mode == "done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete == true) {
+        filterList.push(taskList[i]);
+      }
     }
   }
+  console.log("afterFilter:" + filterList);
+  console.log("afterTask:" + taskList);
+
   render();
+}
+
+function deleteTask(id) {
+  if ((mode = "all")) {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].id == id) {
+        taskList.splice(i, 1);
+        break;
+      }
+    }
+  } else if (mode == "ongoing") {
+    for (let i = 0; i < filterList.length; i++) {
+      if (filterList[i].id == id) {
+        filterList.splice(i, 1);
+        break;
+      }
+    }
+  } else if (mode == "done") {
+    for (let i = 0; i < filterList.length; i++) {
+      if (filterList[i].id == id) {
+        filterList.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  render();
+
+  console.log(filterList);
 }
--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -12,3 +12,5 @@ public class Main {
 //
 // student num N, score[a, b, c , d, e... (N개)
 // print 해라. 각 케이스마다 평균을 넘는 학생의 비율을. 소수점 셋째 자리까지 적용
+
+
